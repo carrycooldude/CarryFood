45e5aaf5a119afff234d9f7f78e03528cb018c9c
carrycooldude <rawatkari554@gmail.com>
2023-06-19 01:35:24 +0530

    CHangded

--- carry-food/src/components/Navbar.js
+++ carry-food/src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-success">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">Navbar</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
